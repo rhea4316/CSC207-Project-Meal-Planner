ac6762fe63b71aa1475fe9889ae47fc3172f27ca
Regina He <reginahe@Reginas-Laptop-2.local>
2025-11-15 15:32:41 -0500

    implemented most of use case for BrowseRecipe

--- src/main/java/com/mealplanner/use_case/browse_recipe/BrowseRecipeInputData.java
+++ src/main/java/com/mealplanner/use_case/browse_recipe/BrowseRecipeInputData.java
@@ -4,6 +4,26 @@ package com.mealplanner.use_case.browse_recipe;
 // Responsible: Regina
 // TODO: Implement with recipe ID or recipe selection criteria from user
 
+import com.mealplanner.entity.Recipe;
+
 public class BrowseRecipeInputData {
+    private final String recipeName;
+    private final String recipeID;
+    //or is recipeID a recipe? idk
+
+    public BrowseRecipeInputData(String name) {
+        this.recipeName = name;
+        this.recipeID = null;
+    }
+
+    public BrowseRecipeInputData(Recipe recipe) {
+        this.recipeName = null;
+        this.recipeID = recipe.getName();
+    }
+
+    //Getters:
+    String getRecipeID() {return recipeID;}
+    String getRecipeName() {return recipeName;}
 
 }
+
--- src/main/java/com/mealplanner/use_case/browse_recipe/BrowseRecipeOutputBoundary.java
+++ src/main/java/com/mealplanner/use_case/browse_recipe/BrowseRecipeOutputBoundary.java
@@ -5,5 +5,8 @@ package com.mealplanner.use_case.browse_recipe;
 // TODO: Define methods for presentRecipeDetails (success) and presentError (failure) cases
 
 public interface BrowseRecipeOutputBoundary {
+    void prepareSuccessView(BrowseRecipeOutputData browseRecipeOutputData);
+
+    void prepareFailureView(String errormessage);
 
 }
