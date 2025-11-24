cd2933a61a85b7f20a43e5c54348b62e7db65926
eden-chang <eden.chang27@gmail.com>
2025-11-23 21:49:00 -0500

    Define AdjustServingSize boundary and data access interfaces

--- src/main/java/com/mealplanner/use_case/adjust_serving_size/AdjustServingSizeInputBoundary.java
+++ src/main/java/com/mealplanner/use_case/adjust_serving_size/AdjustServingSizeInputBoundary.java
@@ -2,8 +2,7 @@ package com.mealplanner.use_case.adjust_serving_size;
 
 // Input boundary interface for adjusting recipe serving size.
 // Responsible: Eden
-// TODO: Define execute method that takes AdjustServingSizeInputData (recipe ID, new serving size) as parameter
 
 public interface AdjustServingSizeInputBoundary {
-
+    void execute(AdjustServingSizeInputData inputData);
 }
