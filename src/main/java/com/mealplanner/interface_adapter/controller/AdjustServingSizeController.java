9aa913820716007f587dc8d556c4e6cb9c6fc6e0
eden-chang <eden.chang27@gmail.com>
2025-11-23 21:49:58 -0500

    Implement AdjustServingSizeController

--- src/main/java/com/mealplanner/interface_adapter/controller/AdjustServingSizeController.java
+++ src/main/java/com/mealplanner/interface_adapter/controller/AdjustServingSizeController.java
@@ -1,9 +1,20 @@
 package com.mealplanner.interface_adapter.controller;
 
+import com.mealplanner.use_case.adjust_serving_size.AdjustServingSizeInputBoundary;
+import com.mealplanner.use_case.adjust_serving_size.AdjustServingSizeInputData;
+
 // Controller for adjusting recipe serving size - receives recipe and new serving count.
 // Responsible: Eden
-// TODO: Implement execute method that converts recipe ID and new serving size to InputData and calls interactor
 
 public class AdjustServingSizeController {
+    private final AdjustServingSizeInputBoundary interactor;
 
+    public AdjustServingSizeController(AdjustServingSizeInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    public void execute(String recipeId, int newServingSize) {
+        AdjustServingSizeInputData inputData = new AdjustServingSizeInputData(recipeId, newServingSize);
+        interactor.execute(inputData);
+    }
 }
