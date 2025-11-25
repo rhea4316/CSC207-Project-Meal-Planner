e82ece0cce478ba0c03117dc9612b71f7bc08720
monaelyass <monaelyass06@gmail.com>
2025-11-25 15:31:10 -0500

    Implemented ViewScheduleController

--- src/main/java/com/mealplanner/interface_adapter/controller/ViewScheduleController.java
+++ src/main/java/com/mealplanner/interface_adapter/controller/ViewScheduleController.java
@@ -2,8 +2,22 @@ package com.mealplanner.interface_adapter.controller;
 
 // Controller for viewing user's meal schedule - receives user identifier and calls interactor.
 // Responsible: Mona
-// TODO: Implement execute method that converts username/user ID to InputData and calls view schedule interactor
+// done: Implement execute method that converts username/user ID to InputData and calls view schedule interactor
+
+import com.mealplanner.use_case.view_schedule.ViewScheduleInputBoundary;
+import com.mealplanner.use_case.view_schedule.ViewScheduleInputData;
 
 public class ViewScheduleController {
+
+    private final ViewScheduleInputBoundary interactor;
+    public ViewScheduleController(ViewScheduleInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    public void execute(String username) {
+        ViewScheduleInputData inputData = new ViewScheduleInputData(username);
+        interactor.execute(inputData);
+    }
+
 
 }
